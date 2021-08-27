a9f23d91b5ed6ca365b5b77543a3892ba7801e6f
Hugo Leyva <hugoleyva2001@icloud.com>
2021-08-27 14:33:01 -0500

    first commit

---
+++ Lectura_Entrada_de_datos.cpp
@@ -0,0 +1,22 @@
+//Lectura o entrada de datos
+
+#include<iostream>  //importa la libreria que se necesita en c++
+
+using namespace std;  //para que permita la salida de datos mas facil: cout
+
+int main(){
+	
+	int numero; //Declaramos la variable numero
+	
+	cout<<"Digite un numero";  //salida imprime digite un numero
+	cin>>numero;        //guarda la variable
+	
+	cout<<"\nEl numero que digito es: "<<numero;
+	
+	
+}
+
+/*asdasd
+asdasd
+asdasd
+asdas*/
---
+++ tipos de datos.cpp
@@ -0,0 +1,16 @@
+//Tipos de datos C++
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int entero = 15;
+	float flotante = 10.5;
+	double mayor = 16.3465;
+	char caracter = 'a';
+	
+	cout<<letra;
+	
+	return 0;
+}
